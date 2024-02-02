27afdb09cce35d526a45714264d4d1a7c6a51b69
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-02-02 15:13:24 -0500

    refactor PlayerCharacter

--- src/Server/Components/PlayerCharacter.lua
+++ src/Server/Components/PlayerCharacter.lua
@@ -8,29 +8,29 @@ local RemoteComponent = require(Packages.RemoteComponent)
 local Knit = require(Packages.Knit)
 
 local PlayerCharacter = Component.new {
-  Tag = "RR_PlayerCharacter",
-  Extensions = { RemoteComponent }
+	Tag = "RR_PlayerCharacter",
+	Extensions = { RemoteComponent },
 }
 PlayerCharacter.RemoteNamespace = PlayerCharacter.Tag
 PlayerCharacter.Client = {
-  PlayEmote = Knit.CreateSignal()
+	PlayEmote = Knit.CreateSignal(),
 }
 
 function PlayerCharacter:PlayEmote(EmoteId: string, Emote: table)
-  if not self.Player then return end
-  self.Client.PlayEmote:Fire(self.Player, EmoteId, Emote)
-  if Emote.PlayedCallback then
-    Emote.PlayedCallback(self.Player, EmoteId, Emote)
-  end
+	if self.Player then
+		self.Client.PlayEmote:Fire(self.Player, EmoteId, Emote)
+
+		if Emote.PlayedCallback then
+			Emote.PlayedCallback(self.Player, EmoteId, Emote)
+		end
+	end
 end
 
-function PlayerCharacter:Start()
-  
-end
+function PlayerCharacter:Start() end
 
 function PlayerCharacter:Construct()
-  self.Player = Players:GetPlayerFromCharacter(self.Instance)
-  self.Humanoid = self.Instance:WaitForChild("Humanoid")
+	self.Player = Players:GetPlayerFromCharacter(self.Instance)
+	self.Humanoid = self.Instance:WaitForChild("Humanoid")
 end
 
 return PlayerCharacter
