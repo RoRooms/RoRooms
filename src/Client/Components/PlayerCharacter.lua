a15975359beac0338a49ed8565899317943106a4
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-02-02 13:16:12 -0500

    refactor PlayerCharacter

--- src/Client/Components/PlayerCharacter.lua
+++ src/Client/Components/PlayerCharacter.lua
@@ -7,30 +7,35 @@ local Component = require(Packages.Component)
 local RemoteComponent = require(Packages.RemoteComponent)
 
 local PlayerCharacter = Component.new {
-  Tag = "RR_PlayerCharacter",
-  Extensions = { RemoteComponent }
+	Tag = "RR_PlayerCharacter",
+	Extensions = { RemoteComponent },
 }
 PlayerCharacter.RemoteNamespace = PlayerCharacter.Tag
 
 function PlayerCharacter:_PlayEmote(EmoteId: string, Emote: table)
-  if self.Player ~= Players.LocalPlayer then return end
-  if not self.Humanoid then return end
-  local HumanoidDescription = self.Humanoid.HumanoidDescription
-  if not HumanoidDescription:GetEmotes()[EmoteId] then
-    HumanoidDescription:AddEmote(EmoteId, string.gsub(Emote.Animation.AnimationId, "%D+", ""))
-  end
-  self.Humanoid:PlayEmote(EmoteId)
+	if self.Player ~= Players.LocalPlayer then
+		return
+	end
+	if not self.Humanoid then
+		return
+	end
+
+	local HumanoidDescription = self.Humanoid.HumanoidDescription
+	if not HumanoidDescription:GetEmotes()[EmoteId] then
+		HumanoidDescription:AddEmote(EmoteId, string.gsub(Emote.Animation.AnimationId, "%D+", ""))
+	end
+	self.Humanoid:PlayEmote(EmoteId)
 end
 
 function PlayerCharacter:Start()
-  self.Server.PlayEmote:Connect(function(EmoteId: string, Emote: table)
-    self:_PlayEmote(EmoteId, Emote)
-  end)
+	self.Server.PlayEmote:Connect(function(EmoteName: string, Emote: table)
+		self:_PlayEmote(EmoteName, Emote)
+	end)
 end
 
 function PlayerCharacter:Construct()
-  self.Player = Players:GetPlayerFromCharacter(self.Instance)
-  self.Humanoid = self.Instance:WaitForChild("Humanoid")
+	self.Player = Players:GetPlayerFromCharacter(self.Instance)
+	self.Humanoid = self.Instance:WaitForChild("Humanoid")
 end
 
 return PlayerCharacter
