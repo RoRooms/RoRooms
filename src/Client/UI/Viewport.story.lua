f631403ea411c959a712a4f907fd0ccdb6c1634e
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-04-22 13:59:17 -0400

    Migrate to flipbook's stories system

--- src/Client/UI/Viewport.story.lua
+++ src/Client/UI/Viewport.story.lua
@@ -1,13 +1,33 @@
-local Viewport = require(script.Parent.Viewport)
+local Viewport = function(Props)
+	local ReturnedGuis = {}
+
+	for _, GuiModule in ipairs(script.Parent.ScreenGuis:GetChildren()) do
+		local GuiNameSplit = string.split(GuiModule.Name, ".")
+		local FileSuffix = GuiNameSplit[2]
+
+		if GuiModule:IsA("ModuleScript") and not FileSuffix then
+			local Gui = require(GuiModule)
+			table.insert(
+				ReturnedGuis,
+				Gui {
+					Name = GuiModule.Name,
+					Parent = Props.Target,
+				}
+			)
+		end
+	end
+
+	return ReturnedGuis
+end
 
 return function(Target)
-  local ViewportGuis = Viewport {
-    Target = Target
-  }
+	local ViewportGuis = Viewport {
+		Target = Target,
+	}
 
-  return function()
-    for _, Gui in ipairs(ViewportGuis) do
-      Gui:Destroy()
-    end
-  end
+	return function()
+		for _, Gui in ipairs(ViewportGuis) do
+			Gui:Destroy()
+		end
+	end
 end
