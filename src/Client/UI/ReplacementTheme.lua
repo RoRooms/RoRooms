6e74c470c61f7c68881f944431d5bf26d099ec8b
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-04-24 12:22:05 -0400

    Add replacement theme

---
+++ src/Client/UI/ReplacementTheme.lua
@@ -0,0 +1,74 @@
+local RoRooms = require(script.Parent.Parent.Parent.Parent.Parent.Parent)
+
+local Shared = RoRooms.Shared
+
+local OnyxUI = require(Shared.ExtPackages.OnyxUI)
+local Colors = require(OnyxUI.Utils.Colors)
+
+return {
+	Colors = {
+		Primary = {
+			Main = Colors.Violet["400"],
+		},
+		Secondary = {
+			Main = Colors.White,
+		},
+		Accent = {
+			Main = Colors.White,
+		},
+
+		Neutral = {
+			Main = Colors.Slate["700"],
+		},
+		NeutralContent = {
+			Main = Colors.Slate["300"],
+		},
+
+		Base = {
+			Main = Colors.Slate["950"],
+		},
+		BaseContent = {
+			Main = Colors.White,
+		},
+
+		Success = {
+			Main = Colors.Green["500"],
+		},
+		Error = {
+			Main = Colors.Red["500"],
+		},
+		Warning = {
+			Main = Colors.Amber["500"],
+		},
+		Info = {
+			Main = Colors.Cyan["400"],
+		},
+	},
+	Font = {
+		Body = "rbxasset://fonts/families/FredokaOne.json",
+		Heading = "rbxasset://fonts/families/FredokaOne.json",
+		Monospace = "rbxasset://fonts/families/FredokaOne.json",
+	},
+	FontWeight = {
+		Body = Enum.FontWeight.Regular,
+		Bold = Enum.FontWeight.Regular,
+		Heading = Enum.FontWeight.Regular,
+	},
+	TextSize = {
+		Base = 16,
+	},
+	CornerRadius = {
+		Base = 10,
+	},
+	StrokeThickness = {
+		Base = 2,
+	},
+	Spacing = {
+		Base = 18,
+	},
+	SpringSpeed = {
+		Base = 25,
+	},
+	SpringDampening = 1,
+	Sound = {},
+}
