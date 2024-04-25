7080b9bff3c7fc48f47b8b36e378719b7c2ab6c9
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-04-25 15:34:20 -0400

    Reorganize WorldsService and WorldRegistryService

---
+++ src/Server/Worlds/WorldRegistryService.lua
@@ -0,0 +1,93 @@
+local InsertService = game:GetService("InsertService")
+local MarketplaceService = game:GetService("MarketplaceService")
+
+local RoRooms = require(script.Parent.Parent.Parent.Parent.Parent.Parent)
+local Signal = require(RoRooms.Packages.Signal)
+
+local REGISTRY_UPDATE_DELAY = 10 * 60
+local REGISTRY_ASSET_ID = 16007140043
+
+local WorldRegistryService = {
+	Name = "WorldRegistryService",
+
+	WorldRegistry = {},
+	WorldRegistryLastUpdated = 0,
+
+	RegistryUpdated = Signal.new(),
+}
+
+function WorldRegistryService:IsWorldRegistered(PlaceId: number)
+	return self.WorldRegistry[PlaceId] ~= nil
+end
+
+function WorldRegistryService:UpdateRegistry()
+	local UpdateStamp = self:_FetchRegistryLastUpdated()
+
+	if UpdateStamp ~= self.WorldRegistryLastUpdated then
+		self.WorldRegistry = self:_FetchLatestRegistry()
+		self.WorldRegistryLastUpdated = UpdateStamp
+
+		self.RegistryUpdated:Fire(self.WorldRegistry)
+	end
+end
+
+function WorldRegistryService:_FetchRegistryLastUpdated()
+	local Success, Result = pcall(function()
+		return MarketplaceService:GetProductInfo(REGISTRY_ASSET_ID).Updated
+	end)
+	if Success then
+		return Result
+	else
+		warn(Result)
+
+		return nil
+	end
+end
+
+function WorldRegistryService:_FetchLatestRegistry()
+	local Success, Result = pcall(function()
+		local ModuleScript = InsertService:LoadAsset(REGISTRY_ASSET_ID):GetChildren()[1]
+
+		if ModuleScript:IsA("ModuleScript") then
+			local Data = require(ModuleScript)
+
+			if typeof(Data) == "table" then
+				return Data
+			else
+				warn("Latest world registry returns a non-table value. Report this ASAP.")
+
+				return {}
+			end
+		else
+			warn("Latest world registry version is not a ModuleScript. Report this ASAP.")
+
+			return {}
+		end
+	end)
+
+	if Success then
+		return Result
+	else
+		warn(Result)
+
+		return nil
+	end
+end
+
+function WorldRegistryService:_SpawnRegistryUpdateLoop()
+	return task.spawn(function()
+		while true do
+			self:UpdateRegistry()
+
+			task.wait(REGISTRY_UPDATE_DELAY)
+		end
+	end)
+end
+
+function WorldRegistryService:KnitStart()
+	self:_SpawnRegistryUpdateLoop()
+end
+
+function WorldRegistryService:KnitInit() end
+
+return WorldRegistryService
--- src/Server/Worlds/WorldsService.lua
+++ src/Server/Worlds/WorldsService.lua
@@ -1,91 +1,97 @@
-local RoRooms = require(script.Parent.Parent.Parent.Parent)
 local DataStoreService = game:GetService("DataStoreService")
-local InsertService = game:GetService("InsertService")
-local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 
-local Shared = RoRooms.Shared
-
-local SharedData = require(Shared.SharedData)
-local ShuffleArray = require(Shared.ExtPackages.ShuffleArray)
-
-local WORLD_REGISTRY_UPDATE_DELAY = 10 * 60
+local RoRooms = require(script.Parent.Parent.Parent.Parent)
+local WorldRegistryService = require(script.Parent.WorldRegistryService)
+local ShuffleArray = require(RoRooms.Shared.ExtPackages.ShuffleArray)
 
 local WorldsService = {
 	Name = "WorldsService",
 	Client = {},
+
+	TopWorlds = {},
+	RandomWorlds = {},
 }
+
+function WorldsService.Client:GetRandomWorlds(PageSize: number, PageCount: number, CurrentPage: number)
+	local Pages = {}
+
+	for Count = 1, PageCount, 1 do
+		local Page = {}
+
+		table.insert(Pages, Page)
+	end
+
+	return Pages
+end
 
 function WorldsService.Client:IsWorldRegistered(_Player: Player, PlaceId: number)
 	if typeof(PlaceId) ~= "number" then
 		return
 	end
-	return WorldsService:IsWorldRegistered(PlaceId)
+
+	return WorldRegistryService:IsWorldRegistered(PlaceId)
 end
 
 function WorldsService.Client:TeleportToWorld(Player: Player, PlaceId: number)
 	if typeof(PlaceId) ~= "number" then
 		return
 	end
+
 	return self.Server:TeleportPlayerToWorld(Player, PlaceId)
 end
 
-function WorldsService:GetWorlds(CurrentPage: number, PageSize: number)
-	local Results = {}
+function WorldsService:TeleportPlayerToWorld(Player: Player, PlaceId: number)
+	if WorldRegistryService:IsWorldRegistered(PlaceId) then
+		TeleportService:Teleport(PlaceId, Player)
 
-	local TopWorldsPageCount = math.ceil(#self.TopWorlds / PageSize)
-	if TopWorldsPageCount == 0 then
-		TopWorldsPageCount = 1
-	end
-
-	if CurrentPage >= TopWorldsPageCount then
-		local PagesPastTopWorlds = CurrentPage - TopWorldsPageCount
-		local CurrentRandomWorldsIndex = PagesPastTopWorlds * PageSize
-
-		for Index = CurrentRandomWorldsIndex, CurrentRandomWorldsIndex + PageSize do
-			table.insert(Results, self.RandomWorldsIndex[Index])
-		end
+		return true
 	else
-		local CurrentTopWorldsIndex = CurrentPage * PageSize
-
-		for Index = CurrentTopWorldsIndex, CurrentTopWorldsIndex + PageSize do
-			table.insert(Results, self.TopWorlds[Index])
-		end
-	end
-
-	return Results
-end
-
-function WorldsService:GetRandomUnchosenWorld()
-	for _, PlaceId in ipairs(self.RandomWorldsIndex) do
-		if table.find(self.Worlds, PlaceId) == nil then
-			return PlaceId
-		end
+		return false, `World {PlaceId} not registered with RoRooms.`
 	end
 end
 
-function WorldsService:StartTopWorldsUpdateLoop()
+function WorldsService:_UpdateRandomWorlds(WorldRegistry: { [string]: { any } })
+	local RandomWorlds = {}
+
+	for PlaceId, _ in pairs(WorldRegistry) do
+		table.insert(RandomWorlds, PlaceId)
+	end
+
+	self.RandomWorlds = ShuffleArray(RandomWorlds)
+end
+
+function WorldsService:_LogWorldTeleport(PlaceId: number)
+	if self:IsWorldRegistered(PlaceId) then
+		self.WorldTeleportsStore:IncrementAsync(tostring(PlaceId), 1)
+	end
+end
+
+function WorldsService:_SpawnTopWorldsUpdateLoop()
 	return task.spawn(function()
 		local Pages = self.PreviousWorldTeleportsStore:GetSortedAsync(false, 100)
-		local AdvancedLastRun = true
+		local AdvancedLastCheck = true
 
 		self.TopWorlds = {}
 
 		while true do
 			local CurrentPage = Pages:GetCurrentPage()
 
-			if AdvancedLastRun then
+			if AdvancedLastCheck then
 				for _, Entry in ipairs(CurrentPage) do
+					print(Entry.value)
+
 					table.insert(self.TopWorlds, Entry.value)
 				end
 			end
 
 			if Pages.IsFinished then
-				AdvancedLastRun = false
+				AdvancedLastCheck = false
 			else
 				Pages:AdvanceToNextPageAsync()
-				AdvancedLastRun = true
+
+				AdvancedLastCheck = true
 			end
 
 			task.wait(5)
@@ -93,101 +99,23 @@ function WorldsService:StartTopWorldsUpdateLoop()
 	end)
 end
 
-function WorldsService:IsWorldRegistered(PlaceId: number)
-	return self.WorldRegistry[PlaceId] ~= nil
-end
-
-function WorldsService:TeleportPlayerToWorld(Player: Player, PlaceId: number)
-	if self:IsWorldRegistered(PlaceId) then
-		TeleportService:Teleport(PlaceId, Player)
-		return true
-	else
-		return false, "World not registered."
-	end
-end
-
-function WorldsService:UpdateRandomWorldsIndex()
-	local RandomWorlds = {}
-	for PlaceId, _ in pairs(self.WorldRegistry) do
-		table.insert(RandomWorlds, PlaceId)
-	end
-	ShuffleArray(RandomWorlds)
-	self.RandomWorldsIndex = RandomWorlds
-end
-
-function WorldsService:UpdateWorldRegistry()
-	local function GetRegistryUpdateStamp()
-		local Success, Result = pcall(function()
-			return MarketplaceService:GetProductInfo(SharedData.WorldRegistryAssetId).Updated
-		end)
-		if Success then
-			return Result
-		else
-			warn(Result)
-		end
-	end
-
-	local function GetRegistryModule()
-		local Success, Result = pcall(function()
-			return InsertService:LoadAsset(SharedData.WorldRegistryAssetId):GetChildren()[1]
-		end)
-		if Success then
-			return Result
-		else
-			warn(Result)
-		end
-	end
-
-	local UpdateStamp = GetRegistryUpdateStamp()
-	if UpdateStamp ~= self.LastWorldRegistryUpdateStamp then
-		self.WorldRegistry = require(GetRegistryModule())
-		self.LastWorldRegistryUpdateStamp = UpdateStamp
-
-		self:UpdateRandomWorldsIndex()
-	end
-end
-
-function WorldsService:StartWorldRegistryUpdateLoop()
-	task.spawn(function()
-		while true do
-			self:UpdateWorldRegistry()
-			task.wait(WORLD_REGISTRY_UPDATE_DELAY)
-		end
-	end)
-end
-
 function WorldsService:KnitStart()
-	task.delay(3, function()
-		print(self:GetWorlds(1, 40))
+	self:_SpawnTopWorldsUpdateLoop()
+
+	WorldRegistryService.RegistryUpdated:Connect(function(WorldRegistry: { [string]: {} })
+		self:_UpdateRandomWorlds(WorldRegistry)
 	end)
-
-	self:StartWorldRegistryUpdateLoop()
-
-	self:StartTopWorldsUpdateLoop()
 
 	Players.PlayerAdded:Connect(function(Player)
-		local JoinData = Player:GetJoinData()
-		if self.WorldRegistry[JoinData.SourcePlaceId] then
-			self.WorldTeleportsStore:IncrementAsync(tostring(JoinData.SourcePlaceId), 1)
-		end
+		self:_LogWorldTeleport(Player:GetJoinData().SourcePlaceId)
 	end)
 end
 
 function WorldsService:KnitInit()
-	self.WorldRegistry = {}
-	self.RandomWorldsIndex = {}
-	self.LastWorldRegistryUpdateStamp = nil
-
-	self.Worlds = {}
-	self.TopWorlds = {}
-
 	self.WorldTeleportsStore =
 		DataStoreService:GetOrderedDataStore("WorldTeleports", tostring(math.floor(os.time() / 86400)))
 	self.PreviousWorldTeleportsStore =
 		DataStoreService:GetOrderedDataStore("WorldTeleports", tostring(math.floor(os.time() / 86400) - 1))
-
-	self.WorldTeleportsPages = nil
-	self.WorldTeleportsLastUpdated = nil
 end
 
 return WorldsService
