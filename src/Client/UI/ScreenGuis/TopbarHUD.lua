a29b8277f841f55ca64b67ff676232dff35181b5
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-10-10 13:09:05 -0400

    Add UIDragDetector to TopbarHUD

--- src/Client/UI/ScreenGuis/TopbarHUD.lua
+++ src/Client/UI/ScreenGuis/TopbarHUD.lua
@@ -1,3 +1,5 @@
+local UserInputService = game:GetService("UserInputService")
+
 local RoRooms = script.Parent.Parent.Parent.Parent.Parent
 local OnyxUI = require(RoRooms.Parent.OnyxUI)
 local Fusion = require(RoRooms.Parent.Fusion)
@@ -7,6 +9,7 @@ local Components = require(RoRooms.SourceCode.Client.UI.Components)
 local Children = Fusion.Children
 local Themer = OnyxUI.Themer
 local Peek = Fusion.peek
+local OnEvent = Fusion.OnEvent
 
 return function(Scope: Fusion.Scope<any>, Props)
 	local Scope = Fusion.innerScope(Scope, Fusion, OnyxUI.Util, OnyxUI.Components, Components)
@@ -37,74 +40,90 @@ return function(Scope: Fusion.Scope<any>, Props)
 					40,
 					1
 				),
-				ListEnabled = true,
-				ListPadding = Scope:Computed(function(Use)
-					return UDim.new(0, Use(Theme.Spacing["0.5"]))
-				end),
-				ListHorizontalAlignment = Enum.HorizontalAlignment.Center,
 
 				[Children] = {
 					Scope:Frame {
-						Name = "TopbarButtons",
-						BackgroundColor3 = Theme.Colors.Base.Main,
-						BackgroundTransparency = States.CoreGui.PreferredTransparency,
+						Name = "Dragger",
 						ListEnabled = true,
 						ListPadding = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.Spacing["0.25"]))
-						end),
-						ListFillDirection = Enum.FillDirection.Horizontal,
-						ListVerticalAlignment = Enum.VerticalAlignment.Center,
-						CornerRadius = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.CornerRadius.Full))
-						end),
-						Padding = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.Spacing["0.5"]) / 1.25)
-						end),
-
-						[Children] = {
-							Scope:ForValues(States.Topbar.Buttons, function(Use, Scope, Button)
-								return Scope:TopbarButton(table.clone(Button))
-							end),
-						},
-					},
-					Scope:BaseButton {
-						Name = "PullButton",
-						BackgroundTransparency = States.CoreGui.PreferredTransparency,
-						BackgroundColor3 = Theme.Colors.Base.Main,
-						Visible = Scope:Computed(function(Use)
-							return not (typeof(Use(States.Menus.CurrentMenu)) == "string")
-						end),
-						TextSize = 0,
-						CornerRadius = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.CornerRadius.Full))
-						end),
-						PaddingTop = Scope:Computed(function(Use)
 							return UDim.new(0, Use(Theme.Spacing["0.5"]))
 						end),
-						PaddingLeft = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.Spacing["0.75"]))
-						end),
-						PaddingRight = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.Spacing["0.75"]))
+						ListHorizontalAlignment = Enum.HorizontalAlignment.Center,
+						Padding = Scope:Computed(function(Use)
+							return UDim.new(0, Use(Theme.Spacing["0"]))
 						end),
 						PaddingBottom = Scope:Computed(function(Use)
-							return UDim.new(0, Use(Theme.Spacing["0.5"]))
+							if (not Use(States.Topbar.Visible)) or UserInputService.TouchEnabled then
+								return UDim.new(0, Use(Theme.Spacing["3"]))
+							else
+								return UDim.new(0, Use(Theme.Spacing["1"]))
+							end
 						end),
-						StrokeColor = Theme.Colors.Neutral.Main,
-
-						OnActivated = function()
-							States.Topbar.Visible:set(not Peek(States.Topbar.Visible))
-							States.Menus.CurrentMenu:set(nil)
-						end,
 
 						[Children] = {
 							Scope:Frame {
-								Size = Scope:Computed(function(Use)
-									return UDim2.fromOffset(100, Use(Theme.StrokeThickness["1"]))
+								Name = "TopbarButtons",
+								BackgroundColor3 = Theme.Colors.Base.Main,
+								BackgroundTransparency = States.CoreGui.PreferredTransparency,
+								ListEnabled = true,
+								ListPadding = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.25"]))
 								end),
-								AutomaticSize = Enum.AutomaticSize.None,
-								BackgroundTransparency = 0,
-								BackgroundColor3 = Theme.Colors.BaseContent.Main,
+								ListFillDirection = Enum.FillDirection.Horizontal,
+								ListVerticalAlignment = Enum.VerticalAlignment.Center,
+								CornerRadius = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.CornerRadius.Full))
+								end),
+								Padding = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.5"]) / 1.25)
+								end),
+
+								[Children] = {
+									Scope:ForValues(States.Topbar.Buttons, function(Use, Scope, Button)
+										return Scope:TopbarButton(table.clone(Button))
+									end),
+								},
+							},
+							Scope:BaseButton {
+								Name = "PullButton",
+								BackgroundTransparency = States.CoreGui.PreferredTransparency,
+								BackgroundColor3 = Theme.Colors.Base.Main,
+								Visible = Scope:Computed(function(Use)
+									return not (typeof(Use(States.Menus.CurrentMenu)) == "string")
+								end),
+								TextSize = 0,
+								CornerRadius = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.CornerRadius.Full))
+								end),
+								PaddingTop = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.5"]))
+								end),
+								PaddingLeft = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.75"]))
+								end),
+								PaddingRight = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.75"]))
+								end),
+								PaddingBottom = Scope:Computed(function(Use)
+									return UDim.new(0, Use(Theme.Spacing["0.5"]))
+								end),
+								StrokeColor = Theme.Colors.Neutral.Main,
+
+								OnActivated = function()
+									States.Topbar.Visible:set(not Peek(States.Topbar.Visible))
+									States.Menus.CurrentMenu:set(nil)
+								end,
+
+								[Children] = {
+									Scope:Frame {
+										Size = Scope:Computed(function(Use)
+											return UDim2.fromOffset(100, Use(Theme.StrokeThickness["1"]))
+										end),
+										AutomaticSize = Enum.AutomaticSize.None,
+										BackgroundTransparency = 0,
+										BackgroundColor3 = Theme.Colors.BaseContent.Main,
+									},
+								},
 							},
 						},
 					},
@@ -113,8 +132,8 @@ return function(Scope: Fusion.Scope<any>, Props)
 		},
 	}
 
-	local TopbarButtons = TopbarInstance.Topbar.TopbarButtons
-	local TopbarPully = TopbarInstance.Topbar.PullButton
+	local TopbarButtons = TopbarInstance.Topbar.Dragger.TopbarButtons
+	local TopbarPully = TopbarInstance.Topbar.Dragger.PullButton
 
 	local function UpdateTopbarBottomPos()
 		States.Topbar.YPosition:set(TopbarPully.AbsolutePosition.Y)
@@ -139,6 +158,33 @@ return function(Scope: Fusion.Scope<any>, Props)
 			Fusion.doCleanup(CleanupHolder)
 		end,
 	})
+
+	local function AddDragDetector()
+		local StartY = Vector2.new()
+		local EndY = Vector2.new()
+
+		Scope:New "UIDragDetector" {
+			Parent = TopbarInstance.Topbar.Dragger,
+			BoundingUI = TopbarInstance.Topbar,
+			DragStyle = Enum.UIDragDetectorDragStyle.Scriptable,
+			DragAxis = Vector2.new(0, 1),
+
+			[OnEvent "DragStart"] = function(Position: Vector2)
+				StartY = Position.Y
+			end,
+			[OnEvent "DragEnd"] = function(Position: Vector2)
+				EndY = Position.Y
+
+				States.Topbar.Visible:set((StartY - EndY) < 0)
+				States.Menus.CurrentMenu:set(nil)
+				States.Menus.ItemsMenu.Open:set(false)
+
+				TopbarInstance.Topbar.Dragger.Position = UDim2.new()
+			end,
+		}
+	end
+
+	AddDragDetector()
 
 	return TopbarInstance
 end
