39feaaf1eb7d7a66f3329dfee9a5003b92a34483
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-11-01 14:46:32 -0400

    Add API docs

--- src/API/init.lua
+++ src/API/init.lua
@@ -5,3 +5,37 @@ if RunService:IsServer() then
 else
 	return require(script.Client)
 end
+
+--[=[
+	@class RoRooms
+]=]
+
+--[=[
+	@method Start
+	@within RoRooms
+
+	Starts RoRooms.
+]=]
+
+--[=[
+	@method Configure
+	@within RoRooms
+
+	@param Config Config
+
+	Reconciles RoRooms' config to your liking.
+
+	[`Config` type available here](https://github.com/RoRooms/RoRooms/blob/main/src/API/Config.lua)
+]=]
+
+--[=[
+	@method Prompt
+	@within RoRooms
+	@client
+
+	@param Prompt Prompt
+
+	Prompts the client, with support for callback buttons.
+
+	[`Prompt` type available here](https://github.com/RoRooms/RoRooms/blob/main/src/Client/UI/States/Prompts.lua)
+]=]
